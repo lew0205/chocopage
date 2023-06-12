fd58f834f1c13ed37faa696fee36db9f05bab370
이은우 <80537533+lew0205@users.noreply.github.com>
2023-06-12 21:09:07 +0900

    Initial commit

---
+++ src/main/java/com/holu/chocopage/ChocoPageApplication.java
@@ -0,0 +1,13 @@
+package com.holu.chocopage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holu/chocopage/KakapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holu.chocopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
